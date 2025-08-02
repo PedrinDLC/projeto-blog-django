ab31e5a281e21a2338511d21768d56af2133ff6a
Pedro Nascimento <pedrohenriqueluiz3@gmail.com>
2025-08-02 19:36:46 -0300

    Usando field validators, sobrescrevendo save do Model e redimensionando imagens

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from utils.images import resize_image
+from utils.model_validators import validate_png
 
 
-# Create your models here.
 class MenuLink(models.Model):
     class Meta:
         verbose_name = "Menu Link"
@@ -29,12 +30,31 @@ class SiteSetup(models.Model):
 
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
+
     show_header = models.BooleanField(default=True)
     show_search = models.BooleanField(default=True)
     show_menu = models.BooleanField(default=True)
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to="assets/favicon/%Y/%m/",
+        blank=True,
+        default="",
+        validators=[validate_png],
+    )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
