fe3a5bc4a45bbc477614d5e39e61f25f6707a684
Pedro Nascimento <pedrohenriqueluiz3@gmail.com>
2025-08-04 17:54:44 -0300

    Usando o context processor dentro dos templates para config

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -2,9 +2,8 @@ from site_setup.models import SiteSetup
 
 
 def site_setup(request):
-    data = SiteSetup.objects.order_by("-id").first()
+    setup = SiteSetup.objects.order_by("-id").first()
+
     return {
-        "site_setup": {
-            "title": data,
-        },
+        "site_setup": setup,
     }
